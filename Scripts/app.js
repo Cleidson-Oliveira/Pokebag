7cb9221cc5f3e59430e814f602403b7b4b238c10
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-03-03 14:05:46 -0300

    refactor page style

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,179 +1,71 @@
-let url = nextUrl = "https://pokeapi.co/api/v2/pokemon";
+let url = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=40";
+let lastCard;
+let intersectionScrollInfinite;
 
-const acessarApi = async url => {
-    let require = await fetch(url);
-    require = await require.json();
-    if(require){
-        return require;
-    }
-}
+const getPokemons = async () => {
+    const requiredPokes = await fetch(url);
+    const result = await requiredPokes.json();
 
-const montarCarrocel = async () => {
-    let carrocel = document.getElementById("carrocel");
-    let dadosApi = await acessarApi(nextUrl);
+    url = result.next;
+
+    const pokes = result.results
+
     
-    nextUrl = dadosApi.next
-
-    for(let e of dadosApi.results){
-        let pokeInfo = await acessarApi(e.url)
-        createItem(carrocel, pokeInfo, "carrocel-item")
-    };
-}
-
-const search = async () => {
-    let searchResult = document.getElementById("search-result");
-    let textSearched = document.getElementById("textSearched").value;
-    let pokeInfo = await acessarApi(`${url}/${textSearched}`);
-    clear(searchResult);
-    createItem(searchResult, pokeInfo, "poke-finded-conteiner")
-
-}
-
-const showDetail = pokeInfo => {
-    let detailConteiner = document.getElementById("detail-poke");
-    let switchConteiner = document.getElementById("switch-poke-detail");
-    switchConteiner.click();
-    clear(detailConteiner);
-    createItem(detailConteiner, pokeInfo, "detail-poke-conteiner");
-    console.log(pokeInfo)
-}
-
-const createItem = (conteiner, pokeInfo, classe) => {
-    let elementos = [
-        getImage(pokeInfo.sprites.other.home.front_default),
-        getNameId(pokeInfo.name, pokeInfo.id),
-        getType(pokeInfo.types),
-        getAbilities(pokeInfo.abilities),
-        getStats(pokeInfo.stats)
-    ]
-
-    if(pokeInfo){
-        let item = document.createElement("div");
-
-        item.classList.add(classe);
-        item.classList.add(pokeInfo.types[0].type.name);
-
-        item.addEventListener("click", async e => {
-            targetClass = e.target.classList;
-            switch (targetClass[0]) {
-                case "ability":
-                    let abilityInfo = await acessarApi(`https://pokeapi.co/api/v2/ability/${e.target.innerHTML}`)
-                    if (abilityInfo) {
-                        showDetailAbility(abilityInfo);
-                    }
-                    break;
-            
-                default:
-                    showDetail(pokeInfo);
-                    break;
-            }
-        })
-
-        for(let el of elementos){
-            item.appendChild(el);
-        }
-
-        conteiner.appendChild(item);
-    }
-}
-
-const getImage = urlImage => {
-    let image = document.createElement("div");
-
-    image.classList.add("poke-image");
-    image.innerHTML = `<img src='${urlImage}'>`;
-
-    return image;
-}
-
-const getNameId = (name, id) => {
-    let nome = document.createElement("div");
-
-    nome.classList.add("poke-name-id");
-    nome.innerHTML = `<p>N° ${id} ${name.toUpperCase()}</p>`;
-
-    return nome;
-}
-
-const getType = types => {
-    let type = document.createElement("div");
-
-    type.classList.add("poke-types");
-    types.forEach(tipo => {
-        type.innerHTML += `<div><p class='type captalise'>${tipo.type.name}</p></div>`;
-    })
-
-    return type;
-}
-
-const getAbilities = abilities => {
-    let habilidades = document.createElement("div");
-
-    habilidades.classList.add("poke-abilities");
-    abilities.forEach(habilidade => {
-        habilidades.innerHTML += `<div><p class='ability captalise'>${habilidade.ability.name}</p></div>`;
-    })
-
-    return habilidades;
-}
-
-const getStats = stats => {
-    let statsPoke = document.createElement("div");
-
-    statsPoke.classList.add("poke-stats");
-    statsPoke.innerHTML = "<div><p>Stats</p></div>";
-
-    stats.forEach(stat=>{
-        let x = document.createElement("div");
-        
-        x.classList.add(stat.stat.name);
-        x.innerHTML = `<p>${stat.base_stat}</p>`;
-        x.style.height = stat.base_stat + "px";
-
-        statsPoke.appendChild(x);
-    })
-    return statsPoke;
-}
-
-const showDetailAbility = abilityInfo => {
-    let detailConteiner = document.getElementById("detail-ability");
-    let switchConteiner = document.getElementById("switch-ability-detail");
-    switchConteiner.click();
-    clear(detailConteiner);
+    const pokemons = await Promise.all(pokes.map(async poke => {
+        const resp = await fetch(poke.url);
+        return resp.json();
+    }));
     
-    let nome = document.createElement("div");
-    nome.innerHTML = `<p>${abilityInfo.name.toUpperCase()}</p>`;
 
-    let description = document.createElement("div");
-    abilityInfo.effect_entries.forEach(textDescription => {
-        if(textDescription.language.name == "en"){
-            description.innerHTML = `<p>${textDescription.short_effect}</p>`;
+    return pokemons;
+}
+
+const pokesContainer = document.querySelector("#pokes-container");
+
+const showPokeCards = async () => {
+    const loadEl = document.querySelector("#loading-poke-container");
+    loadEl.classList.remove("hidden");
+    const pokemons = await getPokemons();
+    loadEl.classList.add("hidden");
+
+    pokemons.forEach(pokemon => {
+        const types = pokemon.types.map(({type}) => type.name);
+
+        const pokeCardView = `
+            <div class="card ${types[0]}">
+                <header class="card-header">
+                    <img src="${pokemon.sprites.other.home.front_default}" alt="">
+                    <p class="poke-name">${pokemon.id} - ${pokemon.name}</p>
+                </header>
+                <div class="types">
+                    ${types.reduce((acc, cur) => {
+                        return acc + `<p>${cur}</p>`
+                    }, "")}
+                </div>
+            </div>
+        `;
+        pokesContainer.innerHTML += pokeCardView;
+    })
+
+    addEventToShowMorePokemons();
+}
+
+
+const addEventToShowMorePokemons = () => {
+    lastCard = document.querySelector("#pokes-container > div:last-child");
+    
+    if ( intersectionScrollInfinite != undefined ) {
+        intersectionScrollInfinite.disconnect();
+    }
+    
+    intersectionScrollInfinite = new IntersectionObserver((entries) => {
+        if (entries.some(entrie => entrie.isIntersecting)) {
+            showPokeCards();;
         }
-    })
+    });
 
-    let pokes = document.createElement("div");
-
-    abilityInfo.pokemon.forEach(poke => {
-        pokes.innerHTML += `<div><p>${poke.pokemon.name.toUpperCase()}</p></div>`;
-    })
-
-    detailConteiner.appendChild(nome);
-    detailConteiner.appendChild(description);
-    detailConteiner.appendChild(pokes);
+    intersectionScrollInfinite.observe(lastCard);
 
 }
 
-const carrocelScroll = () => {
-    let carrocel = document.getElementById("carrocel");
-    if(Math.round(carrocel.scrollLeft) >= carrocel.scrollWidth - carrocel.offsetWidth){
-        montarCarrocel();
-    }
-}
-
-const clear = ( element ) => {
-    while (element.firstChild) {
-        element.removeChild(element.firstChild)
-    }
-}
-
-montarCarrocel()
+showPokeCards();
