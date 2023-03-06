39024eaac7f281ba51c2c39b3dd6e3ea947bd130
Cleidson Oliveira <cleidsonoliveira00@gmail.com>
2023-03-06 09:30:18 -0300

    improve pokemon page style

--- scripts/pokemonInfo.js
+++ scripts/pokemonInfo.js
@@ -21,24 +21,53 @@ const setView = () => {
     .then(pokeInfo => {
         console.log(pokeInfo)
         const view = `
-            <div class="pokeImageAndName">
-                <img src="${pokeInfo.sprites.other.home.front_default}" class="poke-image">
-                <p>${pokeInfo.id} - ${pokeInfo.name}</p>
+            <div class="${pokeInfo.types[0].type.name}">
+                <div class="pokeInfo">
+                    <div>
+                        <h1 class="pokeName">${pokeInfo.name}<span>#${pokeInfo.id}</span></h1>
+                        <div class="pokeTypes">
+                            ${pokeInfo.types.reduce((acc, cur) => {
+                                const type = `<p>${cur.type.name}</p>`;
+
+                                return acc + type
+                            }, "")}
+                        </div>
+                    </div>
+                    <div class="pokeStats" >
+                        <h2>Stats</h2>
+
+                        ${pokeInfo.stats.reduce((acc, cur) => {
+                            const stat = `
+                                <div class="stat" >
+                                    ${cur.stat.name}
+                                    <div
+                                        class="${cur.stat.name}"
+                                        style="width: ${cur.base_stat*3}px"
+                                        data-stat="${cur.base_stat}"
+                                    ></div>
+                                </div>`
+                            return acc + stat;
+                        }, "")}
+                    </div>
+                    <div class="pokeAbility" >
+                        <h2>Abilities</h2>
+
+                        ${pokeInfo.abilities.reduce((acc, cur) => {
+                            const abilities = `
+                                <div>
+                                    ${cur.ability.name}
+                                </div>
+                            `;
+                            return acc + abilities;
+                        }, "")}
+                    </div>
+                </div>
+                <div class="pokeImageContainer">
+                    <img src="${pokeInfo.sprites.other.home.front_default}" class="pokeImage">
+                </div>
             </div>
-            <div class="stats" >
-                ${pokeInfo.stats.reduce((acc, cur) => {
-                    const stat = `
-                        <div class="stat" >
-                            ${cur.stat.name}
-                            <div
-                                class="${cur.stat.name}"
-                                style="width: ${cur.base_stat*3}px"
-                                data-stat="${cur.base_stat}"
-                            ></div>
-                        </div>`
-                    return acc + stat;
-                }, "")}
-            </div>
+            
+
         `;
         container.innerHTML = view
     })
